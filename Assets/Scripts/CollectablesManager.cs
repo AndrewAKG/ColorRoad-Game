0b79ec2f03c2aa2804cb546b3ba37c6e7fe16e1c
TheDreamHunter <andrew.kamal@student.guc.edu.eg>
2018-11-05 13:41:54 +0200

    fix: fix destroyer size

--- Assets/Scripts/CollectablesManager.cs
+++ Assets/Scripts/CollectablesManager.cs
@@ -24,7 +24,7 @@ public class CollectablesManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(collectablesSoFar < amountOfCollectables)
+        while(collectablesSoFar < amountOfCollectables)
         {
             SpawnCollectable();
         }
